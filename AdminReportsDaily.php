07cbd3798e4c6f1fb4cf1520240d5311da5f5ed5
CristobalParaon25 <paraoncristobal25@gmail.com>
2024-01-29 14:29:27 +0800

    edited the table added some rows
    the rows are time in time out

--- AdminReportsDaily.php
+++ AdminReportsDaily.php
@@ -70,63 +70,72 @@ if(!isset($_SESSION["username"])) {
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-                <div class="container-fluid"> 
-                    <!-- Page Heading --> 
-                    <h1 class="h3 text-gray-800 mb-4">Daily Logs Lists</h1> 
-                    <!-- Content Row -->
-                    <div>
-                        <a href="report.php" class="btn btn-primary mb-3">Print All</a>
-                    </div>
-                    <div class="row pl-1 pr-1">
-                        <div class="col col-lg-12">
-                            <!-- Search Bar -->
-                            <div class="input-group mb-3">
-                                <input type="text" id="searchInput" class="form-control" placeholder="Search by Employee ID">
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary" type="button" onclick="searchTable()">Search</button>
-                                </div>
-                            </div>
+                <div class="container-fluid">
+    <!-- Page Heading -->
+    <h1 class="h3 text-gray-800 mb-4">Daily Logs Lists</h1>
+    <!-- Content Row -->
+    <div>
+        <a href="report.php" class="btn btn-primary mb-3">Print All</a>
+    </div>
+    <div class="row pl-1 pr-1">
+        <div class="col col-lg-12">
+            <!-- Search Bar -->
+            <div class="input-group mb-3">
+                <input type="text" id="searchInput" class="form-control" placeholder="Search by Employee ID">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button" onclick="searchTable()">Search</button>
+                </div>
+            </div>
 
-                            <!-- Table -->
-                            <table class="table table-striped table-bordered" id="myTable" style="width: 100%;">
-                                <thead>
-                                    <tr>
-                                        <th class="font-weight-bold">NO.</th>
-                                        <th class="font-weight-bold">EMPLOYEE ID</th>
-                                        <th class="font-weight-bold">DATE</th>
-                                        <th class="font-weight-bold">TIME</th>
-                                        <th class="font-weight-bold">STATUS</th>
-                                        <th class="font-weight-bold">ACTIONS</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    require_once "connection.php"; // Include your database connection file
+            <!-- Table -->
+            <table class="table table-striped table-bordered" id="myTable" style="width: 100%;">
+                <thead>
+                    <tr>
+                        <th class="font-weight-bold">NO.</th>
+                        <th class="font-weight-bold">EMPLOYEE ID</th>
+                        <th class="font-weight-bold">DATE</th>
+                        <th class="font-weight-bold">TimeIn (Am)</th>
+                        <th class="font-weight-bold">TimeOut (Am)</th>
+                        <th class="font-weight-bold">TimeIn (Pm)</th>
+                        <th class="font-weight-bold">TimeOut (Pm)</th>
+                        <th class="font-weight-bold">ACTIONS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    require_once "connection.php"; // Include your database connection file
 
-                                    $get_log_details = mysqli_query($conn, "SELECT * FROM employee_log");
+                    $get_log_details = mysqli_query($conn, "SELECT el.ID, el.Employee_ID, el.Employee_Date, el.Employee_Time, el.Employee_Status,
+                                        ei.Employee_FullName, ei.Employee_Department, ei.Employee_Position, ei.Employee_Sex, ei.user_type,
+                                        el.Employee_TimeInAm, el.Employee_TimeOutAm, el.Employee_TimeInPm, el.Employee_TimeOutPm
+                                 FROM employee_log el
+                                 JOIN employee_information ei ON el.Employee_ID = ei.Employee_ID");
 
-                                    $counter = 1;
 
-                                    while ($row = mysqli_fetch_array($get_log_details)) {
-                                        ?>
-                                        <tr>
-                                            <td class="text-gray-700"><?php echo $counter ?></td>
-                                            <td class="text-gray-900"><?php echo $row['Employee_ID'] ?></td>
-                                            <td class="text-gray-700"><?php echo $row['Employee_Date'] ?></td>
-                                            <td class="text-gray-700"><?php echo $row['Employee_Time'] ?></td>
-                                            <td class="text-gray-700"><?php echo $row['Employee_Status'] ?></td>
-                                            <td>
-                                                <a href="report.php?search=<?php echo $row['Employee_ID']; ?>" class="btn btn-primary btn-sm">Print</a>
-                                            </td>
-                                        </tr>
-                                    <?php
-                                        $counter++;
-                                    }
-                                    ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                    $counter = 1;
+
+                    while ($row = mysqli_fetch_array($get_log_details)) {
+                        ?>
+                        <tr>
+                            <td class="text-gray-700"><?php echo $counter ?></td>
+                            <td class="text-gray-900"><?php echo $row['Employee_ID'] ?></td>
+                            <td class="text-gray-700"><?php echo $row['Employee_Date'] ?></td>
+                            <td class="text-gray-700"><?php echo $row['Employee_TimeInAm'] ?></td>
+                            <td class="text-gray-700"><?php echo $row['Employee_TimeOutAm'] ?></td>
+                            <td class="text-gray-700"><?php echo $row['Employee_TimeInPm'] ?></td>
+                            <td class="text-gray-700"><?php echo $row['Employee_TimeOutPm'] ?></td>
+                            <td>
+                                <a href="report.php?search=<?php echo $row['Employee_ID']; ?>" class="btn btn-primary btn-sm">Print</a>
+                            </td>
+                        </tr>
+                    <?php
+                        $counter++;
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
     <!-- Include Bootstrap JS (if needed) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -154,8 +163,8 @@ if(!isset($_SESSION["username"])) {
             }
         }
     </script>
+</div>
 
-                </div>
                 <!-- /.container-fluid --> 
             </div>
             <!-- Modal Edit -->
